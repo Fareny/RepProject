0dbfc66ab11b0100150a804c820f3e41c6df5a0c
Alex Markov <pd.fareny@gmail.com>
2022-01-28 22:42:07 +0300

    Вывел в консоль первые 2 цифры

--- lesson02Hard.js
+++ lesson02Hard.js
@@ -1,4 +1,4 @@
 let num = 266219;
 let result = num.toString().split('').reduce((numberOne, numberTwo) => numberOne * numberTwo);
 result = result ** 3;
-console.log(String(result.slice(0, 2)));
+console.log(String(result).slice(0, 2));
