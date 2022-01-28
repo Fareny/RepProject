ba5d793304456baeb2704975125ebbdc230f9779
Alex Markov <pd.fareny@gmail.com>
2022-01-28 22:55:34 +0300

    Добавил переменные и ост

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ console.log(typeof fullPrice);
 console.log(typeof adaptive);
 console.log(screens.length);
 
-console.log("Стоимость верстки экранов " + screenPrice + " рублей/долларов/гривен/юани");
-console.log("Стоимость разработки сайта " + fullPrice + " рублей/долларов/гривен/юани");
+console.log("Стоимость верстки экранов " + screenPrice + " долларов");
+console.log("Стоимость разработки сайта " + fullPrice + " долларов");
 console.log(screens.toLowerCase().split(", "));
-console.log("Процент отката посреднику за работу - " + fullPrice * (rollback / 100));
+console.log("Процент отката посреднику за работу - " + fullPrice * (rollback / 100) + " долларов");
