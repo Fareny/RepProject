cdf13171428cbb98f552705ed504adb70d001def
Alex Markov <pd.fareny@gmail.com>
2022-01-28 17:23:43 +0300

    Добавил переменную

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let title = "RepProject";
 let screens = ("Простые, Сложные, Интерактивные");
 let screenPrice = 300;
-let rollback = Math.random(1, 100); //будет считать от 0 до 99? Или от 1 до 100?
+let rollback = 50;
 let fullPrice = 999999;
 let adaptive = true;
 
@@ -13,4 +13,4 @@ console.log(screens.length);
 console.log(screenPrice + " долларов");
 console.log(fullPrice + " долларов");
 console.log(screens.toLowerCase().split(", "));
-console.log(fullPrice * (rollback / 100)); //процент нужен от с целыми числами?
+console.log(fullPrice * (rollback / 100));
