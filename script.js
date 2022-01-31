b944acea1ec91f303a986e45965c48f4276796c5
Alex Markov <pd.fareny@gmail.com>
2022-01-31 19:38:54 +0300

    Добавил конструкцию условий

---
+++ script.js
@@ -0,0 +1,44 @@
+let title;
+let screens;
+let screenPrice;
+let rollback = 50;
+let fullPrice;
+let adaptive;
+
+title = prompt('Как называется ваш проект?');
+screens = prompt('Какие типы экранов нужно разработать? (Простые, Сложные, Интерактивные)');
+screenPrice = prompt('Сколько будет стоить данная работа? (рублей)');
+parseInt(screenPrice);
+adaptive = confirm('Нужен ли адаптив на сайте?');
+console.log(adaptive);
+
+let service1 = prompt('Какой дополнительный тип услуги нужен?'),
+    servicePrice1 = prompt('Сколько это будет стоить? (рублей)'),
+    service2 = prompt('Какой дополнительный тип услуги нужен?'),
+    servicePrice2 = prompt('Сколько это будет стоить? (рублей)');
+fullPrice = parseInt(screenPrice) + parseInt(servicePrice2) + parseInt(servicePrice2);
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log('Стоимость верстки экранов ' + screenPrice + ' рублей');
+console.log('Стоимость разработки сайта ' + fullPrice + ' рублей');
+console.log(screens.toLowerCase().split(", "));
+rollback = fullPrice * (rollback / 100);
+console.log('процент отката посреднику за работу - ' + rollback + ' рублей');
+
+let servicePercentPrice = fullPrice - rollback;
+console.log(Math.ceil(servicePercentPrice));
+
+if (fullPrice >= 30000) {
+    console.log('Даем скидку в 10%');
+} else if (fullPrice >= 15000 && fullPrice <= 30000) {
+    console.log('Даем скидку в 5%');
+} else if (fullPrice <= 15000 && fullPrice >= 0) {
+    console.log('Скидка не предусмотрена');
+} else {
+    console.log('Что то пошло не так');
+}
+
+
