b8e636dd8ddd808721d3f92cbc5c9de952ebc685
Alex Markov <pd.fareny@gmail.com>
2022-02-01 21:38:10 +0300

    Добавил функцию и условный оператор

--- lesson04Hard.js
+++ lesson04Hard.js
@@ -1,11 +1,13 @@
 let result = prompt('Напиши тут:');
 
 const red = function (button) {
+    button = button.trim();
     if (typeof button !== 'string') {
         alert('В качестве аргумента передана не строка');
     } else if (button.length > 30) {
-        button = button.trim();
         console.log(button.slice(0, 30) + '...');
+    } else {
+        console.log(button);
     }
 };
 red(result);
